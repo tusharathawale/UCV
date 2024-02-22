932d8dd88ea1fa2c535c6b2643a06d6eb9c79bc3
Wang <zw1@mac132276.ornl.gov>
2024-02-22 10:02:40 -0500

    update install script

---
+++ README.md
@@ -0,0 +1,4 @@
+The original code comes from 
+https://gitlab.kitware.com/nrushad2001/vtk-m/-/blob/FiberUncertaintyVis/vtkm/filter/uncertainty/Fiber.h?ref_type=heads
+
+We compile the code separately to make it work based on the latest vtkm
---
+++ install_scripts/settings.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+VTKM_MASTER_REPO=https://gitlab.kitware.com/vtk/vtk-m.git
+VTKM_MASTER_VERSION=master
+
+VTKM_REPO=https://gitlab.kitware.com/nrushad2001/vtk-m.git
+VTKM_VERSION=FiberUncertaintyVis
+
+#using binary file
+KOKKOS_REPO=https://github.com/kokkos/kokkos.git
+KOKKOS_VERSION=3.7.01
