8d703cf9fc6ac10b0a4326e94b374344027482ad
Wang <zw1@mac132276.ornl.gov>
2024-02-29 08:49:50 -0500

    update scripts

---
+++ uncertainty/pythonscript/redsea_scatter.py
@@ -0,0 +1,43 @@
+import vtk
+import os
+import sys
+from vtk.util import numpy_support
+import numpy as np
+import math
+from vtkmodules.vtkCommonDataModel import vtkStructuredPoints
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+
+
+def readDS(fname) :
+    reader = vtk.vtkDataSetReader()
+    reader.SetFileName(fname)
+    reader.ReadAllVectorsOn()
+    reader.ReadAllScalarsOn()
+    reader.Update()
+    ds = reader.GetOutput()
+    return ds
+
+if __name__ == "__main__":
+
+    nparray_mean_curl_file="/Users/zw1/Documents/Datasets/Uncertainty/redsea/curlZ/meanVol/meanVorticity.vtk"
+    nparray_mean_vort="/Users/zw1/Documents/Datasets/Uncertainty/redsea/vorticityMagnitude/meanVol/meanVorticity.vtk"
+    
+    ds_curl = readDS(nparray_mean_curl_file)
+    vtkarray_curl=ds_curl.GetPointData().GetArray("meanVorticity")
+    nparray_curl = numpy_support.vtk_to_numpy(vtkarray_curl)
+
+    ds_vort = readDS(nparray_mean_vort)
+    vtkarray_vort=ds_vort.GetPointData().GetArray("meanVorticity")
+    nparray_vort = numpy_support.vtk_to_numpy(vtkarray_vort)
+
+    fig, ax = plt.subplots(figsize=(7,5))
+    plt.scatter(nparray_curl, nparray_vort, s=0.5, alpha=0.1)
+    #plt.hist2d(nparray_n,nparray_i,bins=100)
+    ax.set_xlabel('curlZ',  fontsize=20)
+    ax.set_ylabel('vorticityMagnitude', fontsize=20)
+    ax.tick_params(axis='x', labelsize=20)
+    ax.tick_params(axis='y', labelsize=20)
+
+    plt.savefig("redsea_scatter.png",bbox_inches='tight')
+
--- uncertainty/pythonscript/supernova_scatter.py
+++ uncertainty/pythonscript/supernova_scatter.py
@@ -21,19 +21,39 @@ def readDS(fname) :
 if __name__ == "__main__":
 
     folder_name = sys.argv[1]
+
+
+    #go through all ens and compute the average values
+    ens_num=64
+    nparray_summ_n=None
+    nparray_summ_i=None
+    for i in range(0,ens_num,1):
+        file_name = folder_name+"/ens_"+str(i)+".vtk"
+        print("load file",file_name)
+
+        ds = readDS(file_name)
+
+        # get two field
+        point_array_nickel = ds.GetPointData().GetArray("Nickel")
+        point_array_iron = ds.GetPointData().GetArray("Iron")
+
+        nparray_n = numpy_support.vtk_to_numpy(point_array_nickel)
+        nparray_i = numpy_support.vtk_to_numpy(point_array_iron)
+
+        if i==0:
+            nparray_summ_n=np.zeros(nparray_n.size)
+            nparray_summ_i=np.zeros(nparray_i.size)
     
-    ds = readDS(file_name)
+        nparray_summ_n=np.add(nparray_summ_n,nparray_n)
+        nparray_summ_i=np.add(nparray_summ_i,nparray_i)
 
-    # get two field
-    point_array_nickel = ds.GetPointData().GetArray("Nickel")
-    point_array_iron = ds.GetPointData().GetArray("Iron")
-
-    nparray_n = numpy_support.vtk_to_numpy(point_array_nickel)
-    nparray_i = numpy_support.vtk_to_numpy(point_array_iron)
-    fig, ax = plt.subplots(figsize=(7,6))
-    plt.scatter(nparray_i, nparray_n, s=0.5, alpha=0.1)
+    fig, ax = plt.subplots(figsize=(7,5))
+    plt.scatter(np.divide(nparray_summ_i, ens_num), np.divide(nparray_summ_n,ens_num), s=0.5, alpha=0.1)
     #plt.hist2d(nparray_n,nparray_i,bins=100)
-    ax.set_xlabel('Iron',  fontsize='large')
-    ax.set_ylabel('Nickel', fontsize='large')
+    ax.set_xlabel('Iron',  fontsize=20)
+    ax.set_ylabel('Nickel', fontsize=20)
+    ax.tick_params(axis='x', labelsize=20)
+    ax.tick_params(axis='y', labelsize=20)
+
     plt.savefig("supernova_scatter.png",bbox_inches='tight')
 
